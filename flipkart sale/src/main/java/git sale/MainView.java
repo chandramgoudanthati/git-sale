1f60f57c4ce7e78ca23e1c1fd5c1993567fc8db7
chandram <a.chandram475@gmail.com>
2022-09-20 23:16:24 +0530

    git sales completed

---
+++ flipkart sale/src/main/java/git sale/MainView.java
@@ -0,0 +1,21 @@
+package git sale;
+
+import com.vaadin.flow.component.dependency.HtmlImport;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.notification.Notification;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+@Route
+@HtmlImport("css/shared-styles.html")
+public class MainView extends VerticalLayout {
+
+    public MainView(@Autowired MessageBean bean) {
+        Button button = new Button("Click me",
+                e -> Notification.show(bean.getMessage()));
+        add(button);
+    }
+
+}
---
+++ flipkart-sale/src/main/java/flipkart100/gateway/GatewayApplication.java
@@ -0,0 +1,21 @@
+package flipkart100.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * GiantsGatewayApplication TODO
+ * date time: 2021/9/28 14:59
+ * Copyright 2021 github.com/vencent-lu/giants-gateway-archetype Inc. All rights reserved.
+ *
+ * @author vencent-lu
+ * @since 1.0
+ */
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+}
